aa6babecf272068b76ca75e2cc6a61f57f5cfdd3
Bill Melvin <bill@melvin.org>
2011-01-13 17:00:59 -0500

    Added bmp2jpg.py

---
+++ bmp2jpg.py
@@ -0,0 +1,13 @@
+# bmp2jpg.py 
+# 2011-01-13 bill@melvin.org
+
+import os, Image
+
+print 'Converting BMP to JPG in ' + os.getcwd()
+for f in os.listdir(os.getcwd()):
+    name, ext = os.path.splitext(f)
+    if ext.lower() == ".bmp":
+        outfile = name + ".jpg"
+        print '  ' + f + ' -> ' + outfile
+        Image.open(f).save(outfile)
+print 'Done.'
